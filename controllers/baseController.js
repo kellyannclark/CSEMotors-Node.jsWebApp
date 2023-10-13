3d647947fe6455c142fd760afc44a92df44ff072
kellyannclark <thefaithseed@gmail.com>
2023-10-13 13:10:15 -0600

    changes

---
+++ controllers/baseController.js
@@ -0,0 +1,13 @@
+//The "base" controller will be responsible only for requests to the application 
+//in general, and not to specific areas (such as inventory or accounts).
+
+
+const utilities = require("../utilities/")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
+}
+
+module.exports = baseController
