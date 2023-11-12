d1532d4189ed4acd49c94aaddda2c3bf96f2dab0
kellyannclark <103697379+kellyannclark@users.noreply.github.com>
2023-11-11 18:45:01 -0700

    Add files via upload

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -11,7 +11,20 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 // Route for displaying a specific inventory item
 router.get("/detail/:inv_id", utilities.handleErrors(invController.showInventoryDetail));
 
+//Route for displaying the management view
+router.get("/", utilities.handleErrors(invController.buildManagementView));
 
+// Route to render the add-classification view
+router.get('/add-classification', utilities.handleErrors(invController.addClassificationView));
+
+// Route to handle form submission for adding a new classification
+router.post('/add-classification', utilities.handleErrors(invController.addNewClassification));
+
+// Route to render the add-inventory view
+router.get('/add-inventory', utilities.handleErrors(invController.addInventoryView));
+
+// Route to handle form submission for adding a new inventory
+router.post('/add-inventory', utilities.handleErrors(invController.addInventory));
 
 
 
